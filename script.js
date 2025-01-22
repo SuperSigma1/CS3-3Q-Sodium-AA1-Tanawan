8755971c22b9b68b7dbf5ff4c392987fc13a8be1
SuperSigma1 <ekstanawan22@src.pshs.edu.ph>
2025-01-22 13:49:40 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript Basics</title>
+        
+        <meta charset="UTF-8">
+        <meta name="author" content="Eddie Kurt John S. Tanawan">
+        <meta name="keywords" content="Laboratory 1">
+        <meta name="revised" content="22-01-2025">
+
+        <link rel="icon" href="icon.png" type="image/gif">
+    </head>
+        <body>
+            <script>
+            </script>
+        </body>
+</html>
